b210612c1378eb4161f94d7e2646f0e35f77cb11
StickeyFingersESW9 <koiwai.1059373.me03sf.ilohas@gmail.com>
2018-04-30 13:27:01 +0900

    共通コントローラー用コードの追加

---
+++ Scripts/Tools/XRController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ddd.CoreModule
+{
+	public class XRController : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start()
+		{
+		}
+	}
+}
